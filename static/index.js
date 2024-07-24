ceb20dadca25543ed6e820b85f5121695126b327
Antton Kasslin <antton.kasslin@hotmail.com>
2024-07-24 05:17:43 +0300

    Visible/hidden -status taken into account also in the counter showing how many ratings (reviews) the place has.

--- static/index.js
+++ static/index.js
@@ -152,7 +152,7 @@ async function initMap(apiServices, starRating) {
                   <ul>${descriptionsHTML}</ul>
                   <h2>comments</h2>
                   <p>
-                    ${ratings_for_restaurant.length} review${ratings_for_restaurant.length === 1 ? '' : 's'}
+                    ${filtered_ratings_for_restaurant.length} review${filtered_ratings_for_restaurant.length === 1 ? '' : 's'}
                     <br>average: ${Math.round(rating_average*100)/100}/5
                     <br> ${starRatingHTML}
                   </p>
